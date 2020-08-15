1e6849aa22e8c4fb94f9fe3bf476d5fdfce95284
Julianne Mendoza <jvdcm825@uw.edu>
2020-08-15 14:21:53 -0700

    created react app

---
+++ Client/video-call-app/src/App.js
@@ -0,0 +1,65 @@
+import React, { Component } from 'react';
+
+
+
+class App extends Component {
+  render() {
+    return(
+      <SignIn />
+    )
+  }
+}
+
+class SignIn extends Component {
+  render() {
+    return(
+      <Container>
+  <Row>
+    <Col>1 of 1</Col>
+  </Row>
+</Container>
+
+
+    )
+  }
+}
+
+class NavBar extends Component {
+  render() {
+    return(
+      <ButtonToolbar>
+        
+      </ButtonToolbar>
+
+    )
+  }
+}
+
+// class Video extends Component {
+//   render() {
+//     return(
+
+
+//     )
+//   }
+// }
+
+// class Transcription extends Component {
+//   render() {
+//     return(
+
+
+//     )
+//   }
+// }
+
+// class Chat extends Component {
+//   render() {
+//     return(
+
+
+//     )
+//   }
+// }
+
+export default App;
