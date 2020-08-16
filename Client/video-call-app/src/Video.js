4c7e01dea1b847ad3f276b04e03a8b6037f0a44d
Julianne Mendoza <jvdcm825@uw.edu>
2020-08-16 14:02:18 -0700

    added video

--- Client/video-call-app/src/Video.js
+++ Client/video-call-app/src/Video.js
@@ -3,44 +3,21 @@ import React, {Component} from 'react';
 class Video extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            v1:null,
-            v2:null,
-            constraints: { audio: false, video: {width:400, height: 300}},
-            video: document.querySelector('video'),
-            devices: null
-        };
-        
-    }
-
-    getCameraSelection = async() => {
-        devices = await navigator.mediaDevices.enumerateDevices();
-        const videoDevices = devices.filter(device => device.kind === 'videoinput');
-        const options = videoDevices.map(videoDevices => {
-            return '<optionvalue="${videoDevice.deviceID}">${videoDevice.label}</option>>'
-        });
-        cameraOptions.innerHTML = options.join('');
+        this.videoTag = React.createRef();       
     }
 
     componentDidMount() {
-        this.getCameraSelection();
-        this.startStream();
-    }
 
-    startStream = async (constraints) => {
-        const stream = await navigator.mediaDevices.getUserMedia(constraints);
-        handleStream(stream);
-    }
-    
-    handleStream = (stream) => {
-        video.srcObject = stream;
-        
+        navigator.mediaDevices
+            .getUserMedia({video: true})
+            .then(stream => this.videoTag.current.srcObject = stream)
+            .catch(console.log);
     }
 
     render() {
         return(
           <div className="video-container">
-            <video autoplay id="video-canvas"></video>
+            <video ref={this.videoTag} autoPlay></video>
             <canvas id="user-video"></canvas>
           </div>
         )
