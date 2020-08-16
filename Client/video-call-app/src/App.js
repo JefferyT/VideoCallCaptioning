0f41f04b675a86c3ae522f1d7625dec868f38cf0
Julianne Mendoza <jvdcm825@uw.edu>
2020-08-15 19:17:44 -0700

    added changes

--- Client/video-call-app/src/App.js
+++ Client/video-call-app/src/App.js
@@ -1,65 +1,177 @@
 import React, { Component } from 'react';
-
-
-
+import Button from 'react-bootstrap/Button';
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
+ 
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
+import {Route, Switch, NavLink, Redirect} from "react-router-dom";
+import Badge from 'react-bootstrap/Badge';
+import Card from 'react-bootstrap/Card';
+ 
+ 
 class App extends Component {
   render() {
     return(
-      <SignIn />
+      <div>
+        <Switch>
+          <Route exact path="/" component={SignIn}/> 
+          <Route path="/videoCall" component={MainPage}/>
+          <Redirect to="/" />
+        </Switch>
+        
+      </div>
     )
   }
 }
-
+ 
 class SignIn extends Component {
+  handleClick = () => {
+    return <Redirect to='/videoCall' />;
+  }
+
   render() {
     return(
-      <Container>
-  <Row>
-    <Col>1 of 1</Col>
-  </Row>
-</Container>
+      <Jumbotron>
+        <Container>
+          <Row>
+            <Col>Welcome!</Col>
+          </Row>
+          <br></br>
+          <Row>
+            <Col>
+              <Form>
+                <Form.Group controlId="formName">
+                  <Form.Label>Display Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    placeholder="Enter your display name"
+                  />
+                  <Form.Text className="text-muted">
+                    This is so others know who you are.
+                  </Form.Text>
+                </Form.Group>
 
-
+                <Form.Group controlId="formRoomID">
+                  <Form.Label>Room ID</Form.Label>
+                  <Form.Control
+                    type="password"
+                    placeholder="Enter the Room ID"
+                  />
+                </Form.Group>
+                <Form.Text className="text-muted">
+                  The creator of the room should've sent you their Room ID.{' '}
+                  <br></br>
+                </Form.Text>
+                <br></br>
+                <NavLink to="/videoCall" variant="primary">
+                  Join
+                </NavLink>
+              </Form>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
     )
   }
 }
 
+class MainPage extends Component {
+  render() {
+    return (
+      <div>
+        <Container>
+          <Row>
+            <Col md="8">Testing</Col>
+            <Col>
+              <Row>Transcription</Row>
+              <Row><MessageList /></Row>
+            </Col>
+          </Row>
+        </Container>
+        <NavBar />
+      </div>
+    )
+  }
+}
+ 
 class NavBar extends Component {
   render() {
     return(
-      <ButtonToolbar>
-        
-      </ButtonToolbar>
-
+      <Navbar bg="primary" className="fixed-bottom">
+        <ButtonGroup className="audio-video">
+          <Button variant="outline-light">Audio</Button>
+          <Button variant="outline-light">Video</Button>
+        </ButtonGroup>
+ 
+        <ButtonGroup className="chat-transcription mx-auto">
+          <Button variant="outline-light">Chat</Button>
+          <Button variant="outline-light">Transcription</Button>
+        </ButtonGroup>
+ 
+        <ButtonGroup className="end-meeting">
+          <Button variant="outline-light">End Meeting</Button>
+        </ButtonGroup>
+      </Navbar>
     )
   }
 }
+ 
+class Video extends Component {
+  render() {
+    return(
+      <div>
+        
+      </div>
+ 
+    )
+  }
+}
+ 
+class Transcription extends Component {
+  render() {
+    return(
+      <div>
+        <h1></h1>
+      </div>
+ 
+    )
+  }
+}
+ 
+class MessageList extends Component {
+  render() {
+    return (
+      <Jumbotron fluid id="jumbo">
+        <Container id="messageContainer">
+          <Row id="row1">
+            <Col id="col1">
+              <Badge variant="primary">Hi Thomas!</Badge>{' '}
+            </Col>
+            <Col id="Col3"></Col>
+          </Row>
+          <Row>
+            <Col id="Col3"></Col>
+            <Col id="col1">
+              <Badge variant="light">Hey, give me a second.</Badge>{' '}
+            </Col>
+          </Row>
 
-// class Video extends Component {
-//   render() {
-//     return(
+          <br></br>
+          <Form.Group controlId="formRoomID">
+            <Form.Control type="text" placeholder="Enter your message" />
+          </Form.Group>
+        </Container>
 
-
-//     )
-//   }
-// }
-
-// class Transcription extends Component {
-//   render() {
-//     return(
-
-
-//     )
-//   }
-// }
-
-// class Chat extends Component {
-//   render() {
-//     return(
-
-
-//     )
-//   }
-// }
-
+        <Container></Container>
+      </Jumbotron>
+    );
+  }
+}
+ 
 export default App;
+ 
+
--- Client/video-call-app/src/index.js
+++ Client/video-call-app/src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import {BrowserRouter} from 'react-router-dom';
 
 // Importing the Bootstrap CSS
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('root')
+ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById('root')
 );
